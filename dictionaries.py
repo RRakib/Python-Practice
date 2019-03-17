3d6b0ef94f1da710df1c8082147de676ae1a7bec
Rakib Uddin <16103030@iubat.edu>
2019-03-17 10:52:48 +0600

    Unpacking and Dictionaried

---
+++ dictionaries.py
@@ -0,0 +1,28 @@
+#### Dictionaries can store key value pairs. This is similar to the object
+# # Basic
+# frineds = {
+#     "name" : "Nayan Dey",
+#     "age" : 22,
+#     "place" : "Shavar"
+# }
+# print(frineds.get("name" , "User Not Found"))
+#### Exercise 1
+# numbers = {
+#     0 : "Zero",
+#     1 : "One",
+#     2 : "Two",
+#     3 : "Three",
+#     4 : "Four",
+#     5 : "Five",
+#     6 : "Six",
+#     7 : "Seven",
+#     8 : "Eight",
+#     9 : "Nine",
+# }
+# phone = input("Phone Number: ")
+# phoneNumber = ""
+# for number in phone:
+#     phoneNumber += f"{numbers.get(int(number))} "
+# print(phoneNumber)
+
+
---
+++ unpacking.py
@@ -0,0 +1,18 @@
+# # Normal Way
+# numbers = ( 2, 8, 6, 7)
+# num1 = numbers[0]
+# num2 = numbers[1]
+# num3 = numbers[2]
+# num4 = numbers[3]
+# print(num2)
+# -----------------------------------------------------------
+# # Unpacking Tupple
+# numbers = ( 2, 8, 6, 7)
+# num1, num2, num3, num4 = numbers
+# print(num1)
+# -----------------------------------------------------------
+# # Unpacking List
+# numbers = [ 2, 8, 6, 7 ]
+# num1, num2, num3, num4 = numbers
+# print(num1)
+# -----------------------------------------------------------
