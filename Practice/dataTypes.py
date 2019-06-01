f118e439852d1b4c854f5eba6ccd2969e2ed6277
Rakib <16103030@iubat.edu>
2019-06-01 21:56:21 +0600

    dataTypes Revisited

---
+++ Practice/dataTypes.py
@@ -0,0 +1,23 @@
+########### Number
+# number1 = 5
+# number2 = 55
+# print(number2)
+
+########### String
+# string1 = "This is a test string \n"
+# print(string1 * 5)
+
+########### List
+# list1 = [1, 2, "a", "b", 5.5]
+# print(list1)
+# print(list1[2])
+# list1[0] = 5555
+# print(list1[3:])
+# print(list1 * 2)
+
+########### Tuple
+# tuple1 = (1, 2, 3, "a", "b", 5.5)
+# print(tuple1)
+# print(tuple1[0])
+# print(tuple1[0: 2])
+# print(tuple1 * 2)
