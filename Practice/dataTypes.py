26d1717713f4c2bd2ca948cb533512f527901d8e
Rakib <16103030@iubat.edu>
2019-06-01 22:21:52 +0600

    dataTypes Revisited

--- Practice/dataTypes.py
+++ Practice/dataTypes.py
@@ -21,3 +21,21 @@
 # print(tuple1[0])
 # print(tuple1[0: 2])
 # print(tuple1 * 2)
+
+############ Dictionaries
+# dictionaries1 = {'name': 'Rakib Uddin', 'age': 24, 'gender': 'Male'}
+# print(dictionaries1)
+# print(dictionaries1['name'])
+# print(dictionaries1.keys())
+# print(dictionaries1.values())
+
+############ Type Conversion
+# test_number = 5
+# test_string = '5'
+# test_tuple = (1, 2, 3, 4, 'a', 'a', 'g')
+# test_list = [1, 2, 3, 'a', 'g', 'h']
+# print(int(test_number))
+# print(oct(test_number))
+# print(str(test_string))
+# print(list(test_tuple))
+# print(tuple(test_list))
