eec6f68e8fcde273766011c6b62eed1dc78b9d32
Rakib <16103030@iubat.edu>
2019-06-01 23:42:20 +0600

    Working On Operators

--- Practice/operators.py
+++ Practice/operators.py
@@ -19,3 +19,52 @@
 # print(1 <= 2)
 # print(1 == '1')
 
+########## Assignmnet Operators
+# a = 1 + 2
+# print(a)
+# b = 0
+# b += 2
+# print(b)
+# b -= 1
+# print(b)
+# c = 1
+# c *= 6
+# print(c)
+# c /= 3
+# print(c)
+# c %= 2
+# print(c)
+# c = 2
+# c //= 1
+# print(c)
+# c **= 2
+# print(c)
+
+########## Bitwise Operator
+# a = 60
+# b = 13
+# print(bin(a & b))
+# print(bin(a | b))
+# print(bin(a ^ b))
+# print(bin(~a))
+# print(bin(a << 5))
+# print(bin(a >> 3))
+# print(bin(a))
+
+########## Logical Operator
+# a = 'true'
+# b = 'false'
+# print(a and b)
+# print(a or b)
+
+########## Membership Operator
+# testTuple = (1, 2, 3)
+# print(5 in testTuple)
+# print(1 in testTuple)
+# print(5 not in testTuple)
+
+########## Identity Operator
+# testTuple = (1, 2, 3, 4)
+# testTuple1 = (1, 2, 3, 4)
+# print(testTuple is testTuple1)
+# print(testTuple is not testTuple1)
