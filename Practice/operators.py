7abaee2d00e9210ef4a97fad1d64f37fab0b3d20
Rakib <16103030@iubat.edu>
2019-06-01 23:04:50 +0600

    Working On Operators

---
+++ Practice/operators.py
@@ -0,0 +1,21 @@
+########## Arithmetic Operators
+# print(1 + 5)
+# print(10 - 2)
+# print(5 * 10)
+# print(10 / 5)
+# print(15 % 7)
+# print(5 ** 2)
+# print(11 / 3)
+# print(11 // 3)
+# print(int(10 / 5))
+# print(a - b) #Error
+
+########## Comparison Operators
+# print(1 == 2)
+# print(1 == 1)
+# print(1 != 1)
+# print(1 < 2)
+# print(1 >= 1)
+# print(1 <= 2)
+# print(1 == '1')
+
