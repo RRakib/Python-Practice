e8ac0deb70c1268f904fbdafbee1592067018805
Rakib <16103030@iubat.edu>
2019-06-02 21:28:38 +0600

    Decision Constructs

---
+++ Practice/decisionConstructs.py
@@ -0,0 +1,21 @@
+############ If Statement
+# a = 1
+# b = 1
+# if 1:
+#     print('false')
+# if a == b:
+#     print('true')
+
+############ If Else Statement
+# if 0:
+#     print('true')
+# else:
+#     print('false')
+
+############ Else If Statement
+# if 0:
+#     print('true')
+# elif 0:
+#     print('false')
+# else:
+#     print('SFU')
