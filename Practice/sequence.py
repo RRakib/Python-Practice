bca8acfc320629947743d7d4019fb52c3485ee37
Rakib <16103030@iubat.edu>
2019-06-07 23:45:24 +0600

    Sequence

---
+++ Practice/sequence.py
@@ -0,0 +1,30 @@
+############## List
+# list1 = ['zvzxc', 'asd', 'asdf', 'asdf', 'asdf', 'asdf', 'asdf']
+# list2 = [1, 2, 3, 4, 5, 96, 8]
+# print(list1[3:], list2[:3])
+# print(list2[4])
+# list2[4] = 15
+# print("After updating new list:" , list2[4])
+# list2[4] = "Hello"
+# print("After updating new list:" , list2[4])
+# print(list2)
+# del list2[4]
+# print("After deleted new list:", list2)
+# print("Length of the list is:", len(list2))
+# new_list = ['a', 'b', 'c', 'v', 'r'] + [1, 2, 3, 4, 5]
+# print("Concatenated list:", new_list)
+# repeated_list = [1] * 5
+# print(repeated_list)
+# print("Checking if a 'Hello' is in a list or not: ", "Hello" in list2)
+# print("The maximum number in this list is:", max(list2))
+# print("The minimum number in this list is:" , min(list2))
+
+############### Tuples
+# tuple1 = (1, 2, 3, 4, 5, 6, 7)
+# print(tuple1)
+# print("The length of this tuple is:", len(tuple1))
+# print(tuple1[0])
+# concatanated_tuple = (1, 2, 3, 4) + ('a', 'b,', 'c', 'd')
+# print(concatanated_tuple)
+# del concatanated_tuple
+# print(concatanated_tuple)
