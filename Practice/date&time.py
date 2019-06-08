d69ba611843455ca81ed4e0e005edc1902225c4d
Rakib <16103030@iubat.edu>
2019-06-08 23:29:20 +0600

    Date & Time

---
+++ Practice/date&time.py
@@ -0,0 +1,15 @@
+############### Imports
+import time
+import calendar
+
+############### Time
+# print(time.time())
+# print(time.ctime())
+# print(time.localtime())
+# print("Time altzone: ", time.altzone)
+# print(time.asctime(time.localtime()))
+# print(time.localtime()[2], "/", time.localtime()[1], "/", time.localtime()[0])
+
+############### Calender
+# print("Calendar: ", calendar.month(2019, 6))
+# print(calendar.isleap(2019))
