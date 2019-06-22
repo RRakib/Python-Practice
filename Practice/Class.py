df5897f9f8aa6e919922ea9ed1be68788890cc67
Rakib <16103030@iubat.edu>
2019-06-22 23:27:37 +0600

    Inheritence

--- Practice/Class.py
+++ Practice/Class.py
@@ -1,28 +1,41 @@
-#### Class
+################# Class
 # class Shopping:
 #     def buy(self):
 #         print("Buy milk")
-#  
 #     def pay(self):
 #         print("Pay 50$")
-#
-#
 # shop1 = Shopping()
 # shop1.buy()
 # shop1.pay()
 # shop1.order = "Order the milk"
 # print(shop1.order)
-# -----------------------------------------------------------
+# class Body:
+    # body = 'Strong'
+    # def __init__(self, name):
+    #     self.name = name
+    #     print('Calling from body')
+    # def move(self):
+    #     print('Move', self.name)
+# print(body.body)
+# hand = body('Hand')
+# hand.move()
 
+################## Inheritence
+# class Hand(Body):
+#     def __init__(self, name):
+#         self.name = name
+#         print('Calling from hand')
+#     def move_hand(self):
+#         print('Move', self.name, 'Hand')
+# right = Hand('Right')
+# right.move_hand()
+# right.move()
 
-#### Exercise 1
+################## Exercise 1
 # class Person:
 #     def __init__(self , name): #### Constructor Function
 #         self.name = name
 #     def talk(self):
 #         print(f"{self.name} Is Talking")
-#
-#
 # person1 = Person("Rakib")
 # person1.talk()
-# -----------------------------------------------------------
