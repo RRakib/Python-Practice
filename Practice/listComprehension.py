aa1565bb6ee3cd8fcef726ab6e54f5ce748f27ab
Rakib <16103030@iubat.edu>
2019-09-06 22:50:34 +0600

    List Comprehension

---
+++ Practice/listComprehension.py
@@ -0,0 +1,11 @@
+############## List Comprehension
+# list = [x**2 for x in range(0,11)]
+# print(list)
+### List Comprehension Even Number
+# list = [x for x in range(1,21) if x%2==0]
+# print(list)
+### Product of 2 different list
+# a = [1,2,3,4,5]
+# b = [2,3,4,5,6]
+# list = [a[x] * b[x] for x in range(len(a))]
+# print(list)
