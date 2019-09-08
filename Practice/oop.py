23eaf7dea7b8ad4cd8576c0529b9d8f68f63aec3
Rakib <16103030@iubat.edu>
2019-09-08 23:23:58 +0600

    OOP In Python

---
+++ Practice/oop.py
@@ -0,0 +1,9 @@
+################## Class
+# class Product:
+#     def __init__(self,name,product,price):
+#         self.name = name
+#         self.product = product
+#         self.price = price
+#
+# product1 = vars(Product('Business','iPhone',500))
+# print(product1)
