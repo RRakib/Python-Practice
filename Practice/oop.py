2d224deb1745afc2d9e08289e6a0a7f4374966ac
Rakib <16103030@iubat.edu>
2019-09-08 23:43:00 +0600

    Class Object

--- Practice/oop.py
+++ Practice/oop.py
@@ -7,3 +7,18 @@
 #
 # product1 = vars(Product('Business','iPhone',500))
 # print(product1)
+### Avaerage Rating Course
+# class Course:
+#     def __init__(self,name,rating):
+#         self.name = name
+#         self.rating = rating
+#     def average(self):
+#         result = 0
+#         for rate in self.rating:
+#             result += rate
+#         return result/len(self.rating)
+#
+# course1 = Course('Bangla',[5,5,5,5,10])
+# print('Average of',course1.name,'is:', int(course1.average()))
+# course2 = Course('English',[10,10,10,10,10])
+# print('Average of',course2.name ,'is:', int(course2.average()))
