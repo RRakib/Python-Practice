340800da66eb46a14684bc0b5aeb09f46f623af7
Rakib <16103030@iubat.edu>
2019-09-10 23:09:14 +0600

    Inheritance

--- Practice/oop.py
+++ Practice/oop.py
@@ -27,12 +27,47 @@
 # car1 = Car()
 # car1.Engin()
 ################## Encapsulation
-class Student:
-    def __init__(self):
-        self.__id = 16103030
-        self.__name = 'Rakib Uddin'
-    def student_details(self):
-        print(self.__id)
-        print(self.__name)
-student1 = Student()
-student1.student_details()
+# class Student:
+#     def __init__(self):
+#         self.__id = 16103030
+#         self.__name = 'Rakib Uddin'
+#     def student_details(self):
+#         return self.__id,self.__name
+# student1 = Student()
+# print(student1.student_details())
+# print(student1._Student__id)
+### Setter and Getter
+# class Student:
+#     def setId(self,id):
+#         self.id = id
+#     def getId(self):
+#         print(self.id)
+#     def setName(self,name):
+#         self.name = name
+#     def getName(self):
+#         print(self.name)
+# student1 = Student()
+# student1.setId(16103030)
+# student1.setName('Rakib Uddin')
+# student1.getId()
+# student1.getName()
+################## Inheritence
+# class Vehical:
+#     def __init__(self, tire,wheel,type):
+#         self.tire = tire
+#         self.wheel = wheel
+#         self.type= type
+#     def drive(self):
+#         print('Driving a', self.type)
+# class Car(Vehical):
+#     def __init__(self,brand,model,tire,wheel):
+#         Vehical.__init__(self,tire,wheel)
+#         self.brand = brand
+#         self.model = model
+# class Bike(Vehical):
+#     def __init__(self,price,tire,wheel,type):
+#         Vehical.__init__(self,tire,wheel,type)
+#         self.price = price
+# suzuki = Bike(200000,140,'No','Bike')
+# suzuki.drive()
+# print(vars(suzuki))
