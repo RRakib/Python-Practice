fcbf7319223ec9eddf9438ac8ab5a3569270563b
Rakib <16103030@iubat.edu>
2019-09-14 21:38:21 +0600

    File

--- Practice/fileSystem.py
+++ Practice/fileSystem.py
@@ -9,11 +9,30 @@
 # print(path.exists())
 # for file in path.glob("*"):
 #     print(file)
-text = open('test.txt', 'r+')
+# text = open('test.txt', 'r+')
 # text.close()
-print(text)
-print('Check to see the file name: ', text.name)
-print('Wring file:', text.write('Overwritten text'))
-print('Reading the file after writing:', text.read())
-print('Check to see the mode used in the file: ', text.mode)
-print('Check to see if the file is closed or not: ', text.closed)
+# print(text)
+# print('Check to see the file name: ', text.name)
+# print('Wring file:', text.write('Overwritten text'))
+# print('Reading the file after writing:', text.read())
+# print('Check to see the mode used in the file: ', text.mode)
+# print('Check to see if the file is closed or not: ', text.closed)
+
+############### Write On A File
+# f = open('myfile.txt','w')
+# i = input('Enter some text:')
+# f.write(i)
+# f.close()
+
+############### Read On A File
+# f = open('myfile.txt','r')
+# print(f.read())
+# f.close()
+
+############### Write Continuas File
+# f = open('myfile.txt','w')
+# i = ''
+# while i != '$':
+#     i = input('Create your documents: ')
+#     f.write(i)
+# f.close()
