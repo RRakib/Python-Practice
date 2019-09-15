b447d346a3f323d089c54230aac5a9b8bc470d18
Rakib <16103030@iubat.edu>
2019-09-15 23:28:04 +0600

    Regular Expression

---
+++ Practice/regular-expresion.py
@@ -0,0 +1,14 @@
+import re
+
+# ################# Search
+test = 'I am Rakib'
+# result = re.search(r'R\w\w\w\w', test)
+# print(result.group())
+
+# ################# Findall
+# result = re.findall(r'a\w', test)
+# print(result)
+
+################ Match
+# result = re.match(r'R\w\w\w', test)
+# print(result)
