9a10cae684166012c2faf9cecabffbecf773c105
Rakib <16103030@iubat.edu>
2019-09-16 23:23:10 +0600

    Quantifier Regular Expression

--- Practice/regular-expresion.py
+++ Practice/regular-expresion.py
@@ -1,7 +1,7 @@
 import re
 
 # ################# Search
-test = 'I am Rakib'
+test = 'I am Rakib and i am a web developer'
 # result = re.search(r'R\w\w\w\w', test)
 # print(result.group())
 
@@ -18,5 +18,22 @@ test = 'I am Rakib'
 # print(result)
 
 # ################@# Substring
-result = re.sub(r'\s+','-', test)
-print(result)
+# result = re.sub(r'\s+','-', test)
+# print(result)
+
+# ################# Quantifier
+# ### + => Everything afterward
+# result = re.findall(r'a\w+', test)
+# print(result)
+# ### * => 0 or more
+# result = re.findall(r'a\w*', test)
+# print(result)
+# ### ? => 1 character after the first occurrence
+# result = re.findall(r'a\w?', test)
+# print(result)
+# ### {} => Specify
+# result = re.findall(r'a\w{1}', test)
+# print(result)
+# ### {m,n} => Range
+# result = re.findall(r'a\w{1,2}', test)
+# print(result)
