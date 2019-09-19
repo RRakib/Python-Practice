33507f504109374acbc9e7c03cb0bb5b8551be57
Rakib <16103030@iubat.edu>
2019-09-19 23:42:18 +0600

    Sort Date

--- Practice/date&time.py
+++ Practice/date&time.py
@@ -15,9 +15,17 @@ import calendar
 # print(calendar.isleap(2019))
 
 ############### Date and Time
-import time,datetime
-epoctime = time.time()
-print(time.ctime(epoctime))
-today = datetime.datetime.today()
-print(datetime.datetime.today())
-print('{}/{}/{}'.format(today.day,today.month,today.year))
+import time
+from datetime import *
+# epoctime = time.time()
+# print(time.ctime(epoctime))
+# today = datetime.datetime.today()
+# print(datetime.datetime.today())
+# print('{}/{}/{}'.format(today.day,today.month,today.year))
+ldates = []
+ldates.append(date(2019,2,5))
+ldates.append(date(2016,9,5))
+ldates.append(date(2019,1,5))
+ldates.sort()
+for date in ldates:
+    print(date)
